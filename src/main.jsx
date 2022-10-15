67e44c2e9055bc7fc98c169c1be78b68f150604b
Vipin Mishra <vipin@digiqt.com>
2022-10-15 08:46:50 +0530

    testing git hooks

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRouter from "./routes/AppRouter";
 
+// Styles
+import "./styles/app.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppRouter />
