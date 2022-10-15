546ab007754959e6e36494f0aa87c155e6017eaf
Vipin Mishra <vipin@digiqt.com>
2022-10-15 08:20:22 +0530

    testing git hooks

--- src/pages/Example.jsx
+++ src/pages/Example.jsx
@@ -1,7 +1,7 @@
 const Example = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Hello World!</h1>
       <p>
         Edit <code>`pages/Example.jsx`</code> to see the changes.
       </p>
